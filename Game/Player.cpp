0318a8ad92d99bcd525fbce907df18ad18a35da5
拠点斗 <kyotento0507@gmail.com>
2020-10-02 11:36:14 +0900

    プレイヤーの回転に合わせた移動を実装

--- Game/Player.cpp
+++ Game/Player.cpp
@@ -1,16 +1,22 @@
 #include "stdafx.h"
 #include "Player.h"
 #include <assert.h>
+#define _USE_MATH_DEFINES //M_PI 円周率呼び出し
+#include <math.h>
 
 namespace {
 	float characonRadius = 50.f;			//キャラコンの半径。
 	float characonHeight = 100.f;			//キャラコンの高さ。
 
 	float moveSpeed = 20.f;					//プレイヤーの移動速度。
+	float mouseSensitivity = 1.0f;			//マウス感度。
+	float degry;							//マウスの移動量を加算したもの。
+	float radian;
 
 	CVector2 mouseCursorMovePow = CVector2::Zero();		//マウスカーソルの移動量。
 
 	CVector3 movePower;						//キャラコンの移動にかかる力。
+	CVector3 moveSp;
 
 	CQuaternion rotMousePower;				//マウスの回転量。
 }
@@ -68,8 +74,19 @@ void Player::Move()
 		movePower.z -= moveSpeed;
 	}
 
+	movePower.Normalize();
+
+	radian = M_PI / 180 * degry;
+
+	moveSp.z = -sin(radian) * movePower.x * moveSpeed;
+	moveSp.x = cos(radian) * movePower.x * moveSpeed;
+	//スティックの上下入力の処理
+	moveSp.z += cos(radian) * movePower.z * moveSpeed;
+	moveSp.x += sin(radian) * movePower.z * moveSpeed;
+	moveSp.y = 0.0f;
+
 	//キャラコンを移動させる。
-	m_position = m_characon.Execute(movePower, 1.f);
+	m_position = m_characon.Execute(moveSp, 1.f);
 
 	m_skinModelRender->SetPos(m_position);		//プレイヤーの座標を指定する。
 
@@ -82,7 +99,9 @@ void Player::Rotation()
 {
 	mouseCursorMovePow = MouseCursor().GetMouseMove();			//マウスの移動量を取得。
 
-	rotMousePower.SetRotationDeg(CVector3::AxisY(), mouseCursorMovePow.x);		//移動量をもとにY軸回転の力を計算。
+	degry += mouseCursorMovePow.x * mouseSensitivity;			//マウスの向き。
+
+	rotMousePower.SetRotationDeg(CVector3::AxisY(), mouseCursorMovePow.x * mouseSensitivity);		//移動量をもとにY軸回転の力を計算。
 
 	m_rot.Multiply(rotMousePower);			//元のRotationに加算。
 
