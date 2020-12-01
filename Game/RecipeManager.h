13044ad6ef8fe3848fd010040cd1d518f2e5cfde
kbc18b13 <kbc18b13@stu.kawahara.ac.jp>
2020-12-01 18:06:37 +0900

    レシピ読み込み途中

--- Game/RecipeManager.h
+++ Game/RecipeManager.h
@@ -4,11 +4,20 @@
 
 class RecipeManager{
 public:
+
+	static RecipeManager& Instance(){
+		static RecipeManager st_manager;
+		return st_manager;
+	}
+
 	void AddRecipe( std::unique_ptr<Recipe> recipe ){
 		m_recipeMap[recipe->GetWH()].push_back( std::move( recipe ) );
 	}
 
 private:
+	RecipeManager(){}
+	~RecipeManager(){}
+
 	using RecipeVector = std::vector<std::unique_ptr<Recipe>>;
 	std::map<std::pair<int, int>, RecipeVector> m_recipeMap;
 };
