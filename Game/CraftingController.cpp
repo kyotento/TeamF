f2a8a17f0e73b283b57c778bd1c46cf3c9fd167c
kbc18b13 <kbc18b13@stu.kawahara.ac.jp>
2020-12-08 10:19:24 +0900

    クラフト試作品完成

--- Game/CraftingController.cpp
+++ Game/CraftingController.cpp
@@ -7,7 +7,7 @@
 namespace GUI::Controller{
 	CraftingController::CraftingController(
 		Inventory & inventory, Inventory& craftingSlots, std::unique_ptr<ItemStack>& grabed ) :
-		InventoryController(craftingSlots, grabed), m_playerInventory( inventory ){
+		InventoryController( craftingSlots, grabed ), m_playerInventory( inventory ){
 
 	}
 
@@ -19,31 +19,77 @@ namespace GUI::Controller{
 	}
 
 	void CraftingController::OnClickSlot( Event::ClickEvent & event, unsigned slotNo ){
-		if( slotNo == 9 ){
-			
-		} else{
-			//通常のクリック操作。
-			InventoryController::OnClickSlot( event, slotNo );
+		const unsigned resultSlot = 9;
 
-			//クラフト結果を検索。
-			auto& rm = RecipeManager::Instance();
+		if( slotNo == resultSlot ){
+			//完成品スロットをクリックしたときの動作。
+			auto& result = m_inventory.GetItem( resultSlot );
 
-			int itemArray[9];
-			for( int i = 0; i < 9; i++ ){
+			if( result == nullptr ){
+				return;
+			}
+			if( m_grabed == nullptr ){
+				//カーソルが空なら個数0の完成品と同じアイテムを持たせる。
+				m_grabed = std::make_unique<ItemStack>( result->GetItem(), 0 );
+			}
+			if( m_grabed->GetID() != result->GetID() ){
+				return;
+			}
 
+			//アイテム移動の処理
+			const int grabNum = m_grabed->GetNumber();
+
+			const int moveNum = min( 1, m_grabed->GetStackLimit() - grabNum );
+
+			if( moveNum == 0 ){
+				return;
+			}
+
+			m_grabed->SetNumber( grabNum + moveNum );
+			result->SetNumber( result->GetNumber() - moveNum );
+
+			if( result->GetNumber() == 0 ){
+				m_inventory.SetItem( resultSlot, nullptr );
+			}
+
+			//完成品を取ったので、クラフトテーブルに置いてあるアイテムを消費する。
+			for( int i = 0; i <= 8; i++ ){
 				auto& item = m_inventory.GetItem( i );
 
 				if( item ){
-					itemArray[i] = item->GetID();
-				} else{
-					itemArray[i] = enCube_None;
-				}
+					item->SetNumber( item->GetNumber() - moveNum );
 
+					if( item->GetNumber() == 0 ){
+						m_inventory.SetItem( i, nullptr );
+					}
+				}
+			}
+			return;
+		}
+
+		//通常のクリック操作。
+		InventoryController::OnClickSlot( event, slotNo );
+
+		//クラフト結果を検索。
+		auto& rm = RecipeManager::Instance();
+
+		int itemArray[9];
+		for( int i = 0; i < 9; i++ ){
+
+			auto& item = m_inventory.GetItem( i );
+
+			if( item ){
+				itemArray[i] = item->GetID();
+			} else{
+				//nullをアイテム無しを表すenumに置き換える。
+				itemArray[i] = enCube_None;
 			}
 
-			auto result = rm.GetResult( itemArray );
-
-			m_inventory.SetItem( 9, std::move(result) );
 		}
+
+		auto result = rm.GetResult( itemArray );
+
+		m_inventory.SetItem( 9, std::move( result ) );
+
 	}
 }
