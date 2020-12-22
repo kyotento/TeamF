65be73b47826f80f28631a5cc235807b543d303c
komura-athushi <kbc18b09@stu.kawahara.ac.jp>
2020-12-22 11:19:08 +0900

    ブロックの高さあげてみた

--- Game/RandomMapMaker.h
+++ Game/RandomMapMaker.h
@@ -16,6 +16,7 @@ public:
 	//! @param[in,out] chunk このチャンクに生成する。
 	void GenerateChunk( Chunk& chunk );
 	static const int m_maxHeight = 16;			//マップの最大の高さ
+	static const int m_addHeight = 10;
 	static const int m_minHeight = 0;
 	const float GetSeedX2() const
  	{
