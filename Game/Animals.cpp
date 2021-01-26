3a593ea6dd62e509449d26074474104d888910ff
Kit1145114 <kbc18b23@stu.kawahara.ac.jp>
2021-01-26 16:03:19 +0900

    動物の当たり判定できました。

--- Game/Animals.cpp
+++ Game/Animals.cpp
@@ -12,9 +12,9 @@ Animals::Animals(EnEntity enEntity) : Entity(enEntity)
 	//被弾判定用コリジョン。
 	m_damageCollision = std::make_unique<SuicideObj::CCollisionObj>();
 	CVector3 colPos = (m_position.x, m_position.y + Block::WIDTH, m_position.z);		//コリジョン座標。
-	m_damageCollision->CreateCapsule(colPos, m_rot, m_characonRadius, m_characonHeight);
+	m_damageCollision->CreateCapsule(colPos, m_rot, m_characonRadius*3, m_characonHeight*3);
 	m_damageCollision->SetTimer(enNoTimer);				//寿命無し。
-	m_damageCollision->SetName(L"CEnemy");
+	m_damageCollision->SetName(L"CAnimals");
 	m_damageCollision->SetClass(this);					//クラスのポインタを取得。
 	m_damageCollision->SetIsHurtCollision(true);		//自分から判定をとらない。
 	//プレイヤーのインスタンスを取得。
@@ -50,10 +50,10 @@ void Animals::Fall()
 //ジャンプ。
 void Animals::Jump()
 {
-	if (m_characon.IsContactWall() && m_characon.IsOnGround())
-	{
-		m_jumpFlag = true;
-	}
+	//if (m_characon.IsContactWall() && m_characon.IsOnGround())
+	//{
+	//	m_jumpFlag = true;
+	//}
 
 	if (m_jumpFlag) {
 		m_direction.y += m_jmpInitialVelocity;
