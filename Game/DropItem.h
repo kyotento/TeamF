0e52676c991a687074e6f4cc95529833d0cc096a
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2021-02-17 11:55:37 +0900

    DropItem高速化、取得範囲拡大

--- Game/DropItem.h
+++ Game/DropItem.h
@@ -2,15 +2,17 @@
 #include "Entity.h"
 #include "SimpleMoveObj.h"
 #include "ItemType.h"
-
-class ItemStack;
+#include "MCCharaCon.h"
+#include "ItemStack.h"
 
 //! @brief Entity としてワールドに実体化したアイテムを表すクラス。
 //! @details 生成にはstatic関数のCreateDropItemを使用してください。
 class DropItem : public Entity
 {
 public:
-	DropItem();
+	DropItem() {
+		m_tick = MAX_TICK_F / 3 * (CMath::RandomInt()%3);
+	}
 
 	//! @brief 引数のワールドとアイテムを使用してドロップアイテムを作成します。
 	//! @param item 設定するアイテム。右辺値参照なので入れる前にstd::move()が必要です。
@@ -23,7 +25,7 @@ public:
 	void SetItemStack( std::unique_ptr<ItemStack>&& item );
 
 	void SetPos( const CVector3& position ) override{
-		m_collision.SetPos( position );
+		m_charaCon.SetPosition(position);// m_collision.SetPos(position);
 	}
 
 	void SetVelocity( const CVector3& velocity ){
@@ -31,7 +33,7 @@ public:
 	}
 
 	CVector3 GetPos() const override{
-		return m_collision.GetPos();
+		return m_charaCon.GetPosition();// m_collision.GetPos();
 	}
 
 	/// <summary>
@@ -39,7 +41,7 @@ public:
 	/// </summary>
 	/// <param name="enable"></param>
 	void SetEnableEntityInner(bool enable)override {
-		m_collision.GetCollision().SetEnable(enable);
+		//m_collision.GetCollision().SetEnable(enable);
 		m_model.SetEnable(enable);
 	}
 
@@ -47,16 +49,21 @@ public:
 	void Update() override;
 
 private:
-	GameObj::CSkinModelRender m_model;
+	GameObj::CInstancingModelRender m_model;
 	CRayTracingModelRender m_raytraceModel;
 
 	std::unique_ptr<ItemStack> m_itemStack;//アイテム。
 
-	CQuaternion m_rot = CQuaternion::Identity();//回転。
-
 	CVector3 m_velocity = CVector3::Zero(); //速度
 
-	SimpleMoveObj m_collision;//当たり判定。位置もここから取得する。
+	//SimpleMoveObj m_collision;//当たり判定。位置もここから取得する。
+	MCCharaCon m_charaCon;
+
+	bool m_enableCon = true;
+
 	float m_timer = 0.0f;
+	int m_tick = 0;
+
+	static constexpr int MAX_TICK_F = 60;
 };
 
