a1ed44a69b0a2953ccd9912f841938060e4d2eed
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2021-02-22 11:23:51 +0900

    時刻を保存するように

--- Game/BossGenerator.h
+++ Game/BossGenerator.h
@@ -6,6 +6,22 @@ class Sun;
 class BossGenerator : public MobGenerator
 {
 public:
+	BossGenerator() {
+		std::ifstream file("Save/Time/boss.time", std::ios::binary);
+		if (!file) {
+			return;
+		}
+		file.read(reinterpret_cast<char*>(&m_runCnt), sizeof(m_runCnt));
+	}
+	~BossGenerator() {
+		std::filesystem::path path("./Save/Time/boss.time");
+		//ディレクトリが無い場合に作成する。
+		std::filesystem::create_directories(path.parent_path());
+		//セーブ
+		std::ofstream file(path.string(), std::ios::binary);
+		file.write(reinterpret_cast<const char*>(&m_runCnt), sizeof(m_runCnt));
+	}
+
 	void Init() override;
 	void NewGOMob(const CVector3& pos) override;
 	void PreUpdate()override;
